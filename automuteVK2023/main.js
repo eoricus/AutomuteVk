b8fb884cc1113b0a9a739dae07b8555adc4e61a1
eoricus <35727198+erikmikoyan2003@users.noreply.github.com>
2023-01-09 22:58:11 +0300

    Add files via upload

---
+++ automuteVK2023/main.js
@@ -0,0 +1,19 @@
+const blackList = ["531970408", "109701271"] // TODO добавить Тему
+const mute = (event) => {
+    all_divs = div = document.getElementsByClassName("im-mess-stack _im_mess_stack ")
+
+    for (i = 0; i <= all_divs.length; i++){
+        div = all_divs[i];
+        if (div) {
+            console.log(div.getAttribute("data-peer"))
+            if (blackList.includes(div.getAttribute("data-peer"))) {
+                div.parentNode.removeChild(div);
+                console.log("dwdw")
+            }  
+        }
+        // alert(div.getAttribute("data-peer"))
+    }
+};
+
+document.addEventListener('DOMContentLoaded', mute, false);
+document.addEventListener('mouseover', mute, false);
